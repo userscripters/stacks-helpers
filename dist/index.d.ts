7a4ff75100eb3cc1057c70f42338f87f629bf41c
Oleg Valter <o.a.philippov@gmail.com>
2021-08-11 10:08:44 +0300

    added build npm scripts + cleaned dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { StacksToastOptions } from "./types";
-/**
- * @see https://stackoverflow.design/product/components/notices/
- * @summary builder for Stacks notifications
- */
-export declare const makeStacksToast: (id: string, text: string, { buttons, classes, msgClasses }?: StacksToastOptions) => HTMLDivElement;
