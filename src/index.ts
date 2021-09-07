f614946b45dc5ab22c88c8dda6ccf6564a58bb5f
Oleg Valter <o.a.philippov@gmail.com>
2021-09-07 17:40:06 +0300

    reexported helpers from the main module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import * as Icons from "./icons";
+import * as Modals from "./modals";
+import * as Notifications from "./notifications";
+
 export type StacksCommonOptions = {
     classes?: string[];
 };
+
+export { Icons, Modals, Notifications };
