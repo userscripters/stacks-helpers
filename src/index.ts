afffa7ac509d34fdf67bfee131a39a19b9b68beb
double beep <38133098+double-beep@users.noreply.github.com>
2022-05-28 09:05:48 +0000

    support for navigation components

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import * as Buttons from "./buttons";
 import * as Icons from "./icons";
 import * as Modals from "./modals";
 import * as Notifications from "./notifications";
+import * as Navigation from "./navigation";
 
 export type StacksCommonOptions = {
     classes?: string[];
 };
 
-export { Buttons, Icons, Modals, Notifications };
+export { Buttons, Icons, Modals, Notifications, Navigation };
