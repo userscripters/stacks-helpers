a4e9d7bc5d212ee522f30a6204d040176124ecef
double beep <38133098+double-beep@users.noreply.github.com>
2022-05-29 09:59:02 +0000

    add label and input makers

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import * as Icons from "./icons";
 import * as Modals from "./modals";
 import * as Notifications from "./notifications";
 import * as Navigation from "./navigation";
+import * as Input from "./input";
+import * as Label from "./label";
 
 export type StacksCommonOptions = {
     classes?: string[];
 };
 
-export { Buttons, Icons, Modals, Notifications, Navigation };
+export { Buttons, Icons, Modals, Notifications, Navigation, Input, Label };
