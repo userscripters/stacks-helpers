0d72fad20ddb7b452f78a451bccb92e953f149f6
double beep <38133098+double-beep@users.noreply.github.com>
2022-05-29 13:15:15 +0000

    add checkbox/toggle maker

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import * as Notifications from "./notifications";
 import * as Navigation from "./navigation";
 import * as Input from "./input";
 import * as Label from "./label";
+import * as Checkbox from "./checkbox";
+import * as Toggle from "./toggle";
 
 export type StacksCommonOptions = {
     classes?: string[];
 };
 
-export { Buttons, Icons, Modals, Notifications, Navigation, Input, Label };
+export { Buttons, Icons, Modals, Notifications, Navigation, Input, Label, Checkbox, Toggle };
