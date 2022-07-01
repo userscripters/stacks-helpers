b5fd892dfe1eff00288639e74849c4d9f740de0c
double beep <38133098+double-beep@users.noreply.github.com>
2022-07-01 10:04:13 +0000

    import, then export

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,62 @@
-export * as Avatar from "./avatar";
-export * as Badges from "./badges";
-export * as Banners from "./banners";
-export * as Breadcrumb from "./breadcrumb";
-export * as ButtonGroup from "./button-groups";
-export * as Checkbox from "./checkbox";
-export * as Indicator from "./indicator";
-export * as Input from "./input";
-export * as Label from "./label";
-export * as Links from "./links";
-export * as Menu from "./menus";
-export * as Navigation from "./navigation";
-export * as Notice from "./notices";
-export * as Pagination from "./pagination";
-export * as Progress from "./progress";
-export * as Popover from "./popover";
-export * as Radio from "./radio";
-export * as Select from "./select";
-export * as Spinner from "./spinner";
-export * as Tag from "./tags";
-export * as Textarea from "./textarea";
-export * as Toggle from "./toggle";
-export * as UserCard from "./usercards";
+import * as Avatar from "./avatar";
+import * as Badges from "./badges";
+import * as Banners from "./banners";
+import * as Breadcrumb from "./breadcrumb";
+import * as ButtonGroup from "./button-groups";
+import * as Checkbox from "./checkbox";
+import * as Indicator from "./indicator";
+import * as Input from "./input";
+import * as Label from "./label";
+import * as Links from "./links";
+import * as Menu from "./menus";
+import * as Navigation from "./navigation";
+import * as Notice from "./notices";
+import * as Pagination from "./pagination";
+import * as Progress from "./progress";
+import * as Popover from "./popover";
+import * as Radio from "./radio";
+import * as Select from "./select";
+import * as Spinner from "./spinner";
+import * as Tag from "./tags";
+import * as Textarea from "./textarea";
+import * as Toggle from "./toggle";
+import * as UserCard from "./usercards";
 
-export * as Buttons from "./buttons";
-export * as Icons from "./icons";
-export * as Modals from "./modals";
-export * as Notifications from "./notifications";
+import * as Buttons from "./buttons";
+import * as Icons from "./icons";
+import * as Modals from "./modals";
+import * as Notifications from "./notifications";
 
 export type StacksCommonOptions = {
     classes?: string[];
+};
+
+export {
+    Avatar,
+    Badges,
+    Banners,
+    Breadcrumb,
+    ButtonGroup,
+    Checkbox,
+    Indicator,
+    Input,
+    Label,
+    Links,
+    Menu,
+    Navigation,
+    Notice,
+    Pagination,
+    Progress,
+    Popover,
+    Radio,
+    Select,
+    Spinner,
+    Tag,
+    Textarea,
+    Toggle,
+    UserCard,
+    Buttons,
+    Icons,
+    Modals,
+    Notifications,
 };
